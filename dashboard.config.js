38285320be42154f66884cfee67b52eede808ba3
yuassibno <121559759+yuassibno@users.noreply.github.com>
2023-03-04 15:00:38 +0100

    Update dashboard.config.js

--- dashboard.config.js
+++ dashboard.config.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    owners: ["", ""],
-    clientID: "",
+    owners: ["661590373755387935", ""],
+    clientID: "1081575442038931496",
     clientSECRET: "",
     domain: "http://localhost:3030",
     port: 3030
